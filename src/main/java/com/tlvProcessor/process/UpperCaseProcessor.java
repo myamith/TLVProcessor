f3e5aa2b3a1776baaf8f4fc3d5f0c1199c2fb01b
Amith K B <amkb@cisco.com>
2017-04-23 02:04:20 +0530

    fixing dataset

--- src/main/java/com/tlvProcessor/process/UpperCaseProcessor.java
+++ src/main/java/com/tlvProcessor/process/UpperCaseProcessor.java
@@ -17,7 +17,7 @@ public class UpperCaseProcessor implements Processor {
 	public String process(String data, int dataLength) {
 		// TODO Auto-generated method stub
 		if (data.length() > dataLength)
-			return data.toUpperCase().substring(0, dataLength - 1);
+			return data.toUpperCase().substring(0, dataLength);
 		else
 			return data.toUpperCase();
 	}
